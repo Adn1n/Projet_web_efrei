d5be8dd3b845d96e032b2ee24a81b9593a20dc62
Adn1n <adnanmoubarac@icloud.com>
2026-05-10 21:05:32 +0200

    Accueil html + css finis

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// CAROUSEL
+
+let currentSlide = 0;
+
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index){
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+}
+
+function nextSlide(){
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+
+function previousSlide(){
+
+    currentSlide--;
+
+    if(currentSlide < 0){
+        currentSlide = slides.length - 1;
+    }
+
+    showSlide(currentSlide);
+}
+
+// Défilement automatique
+
+setInterval(nextSlide, 5000);
+
+// BOUTON RETOUR EN HAUT
+
+function goTop(){
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+// MENU RESPONSIVE
+
+const menuButton = document.getElementById("menu-button");
+const nav = document.getElementById("main-nav");
+
+menuButton.addEventListener("click", function(){
+
+    if(nav.style.display === "flex"){
+        nav.style.display = "none";
+    }
+    else{
+        nav.style.display = "flex";
+    }
+});
